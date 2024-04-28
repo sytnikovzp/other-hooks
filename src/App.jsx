3136042d15f29e4dd5acab915dd4c8302dcbb001
alex <sytnikov.zp@gmail.com>
2024-04-29 01:51:35 +0300

    Add useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,47 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
 
-// let renCount = 1;
+function freezeCompute(numb, pause) {
+  console.log('Is frozen');
+  let start = new Date().getTime();
+  let end = start;
+  while (end < start + pause) {
+    end = new Date().getTime();
+  }
+  return numb * 2;
+}
 
 function App() {
-	// const [renCount, setRenCount] = useState(1);
-	const [test, setTest] = useState('Ok');
+  const [numb, setNumb] = useState(20);
 
-  // useEffect(() => {
-  //   setRenCount(prev => prev + 1)
-  // })
-  // With global var
-  // useEffect(() => {
-  //   renCount++
-  // })
+  const [colored, setColored] = useState(false);
 
-  // useRef()
-
-  const rCount = useRef(1);
-  const fieldRef = useRef(null);
-  const lastState = useRef('');
-
+  const styles = useMemo(
+    () => ({
+      color: colored ? 'red' : 'teal',
+    }),
+    [colored]
+  );
 
   useEffect(() => {
-    console.dir(rCount)
-    rCount.current++
-  })
+    console.log('Styles have been changed');
+  }, [styles]);
 
-  useEffect(() => {
-    lastState.current = test
-    console.log(lastState.current)
-  })
+  const pause = 1000;
 
-  const handlerFocus = () => {
-    console.dir(fieldRef.current)
-    return fieldRef.current.focus()
-  }
+  // const calculated = freezeCompute(numb, pause)
+  const calculated = useMemo(() => freezeCompute(numb, pause), [numb]);
 
-	return (
-		<div className='app'>
-			<h1>Number of render : <span>{rCount.current}</span></h1>
-			{/* <h2>Count of render: {renCount}</h2> */}
-			<h2>Last state: {lastState.current}</h2>
-			<input
-				ref={fieldRef}
-				type='text'
-				onChange={(e) => setTest(e.target.value)}
-				value={test}
-			/>
-			<button onFocus={handlerFocus}>Focus</button>
-		</div>
-	);
+  return (
+    <div className='app'>
+      <h1 style={styles}>Compute property: {calculated}</h1>
+      <button onClick={() => setNumb((prevNumb) => prevNumb + 1)}>Plus</button>
+      <button onClick={() => setNumb((prevNumb) => prevNumb - 1)}>Minus</button>
+      <button onClick={() => setColored((prevColored) => !prevColored)}>
+        Change color
+      </button>
+    </div>
+  );
 }
 
 export default App;
